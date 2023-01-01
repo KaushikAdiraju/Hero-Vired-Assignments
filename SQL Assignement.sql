dd57714285bfcc33e7a181c8373efbb3938caccb
KaushikAdiraju <95290550+KaushikAdiraju@users.noreply.github.com>
2023-01-01 09:53:43 +0530

    Add files via upload

---
+++ SQL Assignement.sql
@@ -0,0 +1,16 @@
+#create database Employee;
+#create table Employee_Details(
+#Emp_id int primary key not null,
+#Emp_name varchar(50),
+#Job_Name varchar(50),
+#Manager_id int,
+#Hire_date date,
+#Salary int,
+#Dep_id int not null);
+
+#insert into Employee_Details values(6100,'Kaushik','Data Scientist',1001,'2022-10-1',10000,1),(6101,'Rakesh','Data Analyst',1001,'2022-10-1',10000,1),
+#(6102,'Gangadhar','Data Engineer',1002,'2022-10-1',10000,1),(6103,'Thiresh','Data Analyst',1001,'2022-10-1',10000,1),(6104,'Nivesh','Data Scientist',1001,'2022-10-1',10000,1),(6105,'Subbu','Data Engineer',1002,'2022-10-1',10500,2),(6106,'Ravi','Software Developer',1003,'2022-10-1',10000,3),(6107,'Pavan','Full Stack Developer',1003,'2022-10-1',11000,3),(6108,'Guru','Data Engineer',1002,'2022-10-1',10000,3),(6109,'George','Software Engineer',1003,'2022-10-1',11000,3);
+#select * from Employee_Details
+
+
+
